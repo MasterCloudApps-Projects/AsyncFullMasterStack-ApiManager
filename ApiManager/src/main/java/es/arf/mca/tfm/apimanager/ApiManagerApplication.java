1286ce3570c17e626f0068d279d0df71c6f5af7d
Alexis Ramos <alexis.ramosf@gmail.com>
2020-10-01 21:35:56 +0200

    Creado esqueleto de proyecto

---
+++ ApiManager/src/main/java/es/arf/mca/tfm/apimanager/ApiManagerApplication.java
@@ -0,0 +1,13 @@
+package es.arf.mca.tfm.apimanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiManagerApplication.class, args);
+	}
+
+}
