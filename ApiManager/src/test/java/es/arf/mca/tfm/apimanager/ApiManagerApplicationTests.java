701966499063d8a46ea0054c35579a62cfa9d634
Alexis Ramos <alexis.ramosf@gmail.com>
2020-11-07 20:59:17 +0100

    Definir caso de uso y comprobar que falla

--- ApiManager/src/test/java/es/arf/mca/tfm/apimanager/ApiManagerApplicationTests.java
+++ ApiManager/src/test/java/es/arf/mca/tfm/apimanager/ApiManagerApplicationTests.java
@@ -1,13 +1,33 @@
 package es.arf.mca.tfm.apimanager;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
+import io.restassured.RestAssured;
+
+import static io.restassured.RestAssured.when;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DisplayName("REST tests")
 class ApiManagerApplicationTests {
+	
+    @LocalServerPort
+    int port;
 
+    @BeforeEach
+    public void setUp() {
+        RestAssured.port = port;
+    }
+	
 	@Test
-	void contextLoads() {
+	public void getStockValue() {
+		when().
+			get("/api/TSLA").
+		then().
+			assertThat().
+			statusCode(200);
 	}
-
 }
